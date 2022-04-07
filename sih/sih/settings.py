5d0d8baf7c98b4a68db55b13cc90143857415bf3
rahul7011 <champrahul01@gmail.com>
2022-04-07 22:12:41 +0530

    Added Custom User Model

--- sih/sih/settings.py
+++ sih/sih/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'yukti',
+    'accounts', 
 ]
 
 MIDDLEWARE = [
@@ -117,3 +118,6 @@ MEDIA_ROOT = BASE_DIR / 'media_root'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'accounts.User'
